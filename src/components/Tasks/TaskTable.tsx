4c118e2d5c1727703d9fbae1b278a24bee67b18a
Durgesh Kumar <113721815+Durgesh2008@users.noreply.github.com>
2025-01-22 11:30:40 +0530

    Update TaskTable.tsx

--- src/components/Tasks/TaskTable.tsx
+++ src/components/Tasks/TaskTable.tsx
@@ -133,7 +133,7 @@ const TaskTable = () => {
             <ErrorPage type={ErrorType.ServerError} msg={error} />
           ) : (
             <>
-              <table className="w-full  text-sm text-left rtl:text-right text-gray-500 overflow-y-auto dark:text-gray-400">
+              <table className="w-full  text-sm text-left rtl:text-right text-gray-500 overflow-auto dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
                     <th scope="col" className="px-6 py-3">
